0491db5bcd3f61023af3c3855cc500f97e5bbc47
Vinu Sreenivasan <vinu.sreenivasan92@gmail.com>
2018-11-12 03:48:50 -0700

    Adding clomp changes

--- benchmarks/prob/problem.py
+++ benchmarks/prob/problem.py
@@ -3,13 +3,11 @@ class Problem():
     def __init__(self):
         space = OrderedDict()
         #problem specific parameters
-        space['LOOP1'] = ["#pragma unroll","#pragma nounroll","#pragma clang loop id(myloop)"]
-        #space['LOOP2'] = ["#pragma nounroll"]
-        #space['LOOP3'] = ["#pragma clang loop id(myloop)"]
-        #space['LOOP4'] = ["#pragma unroll"]
+        space['LOOP1'] = ["#pragma omp parallel for private (pidx) schedule(static)","#pragma omp parallel for private (pidx) schedule(dynamic)"]
+        space['LOOP2'] = ["#pragma omp parallel for private (pidx) schedule(dynamic)","#pragma omp parallel for private (pidx) schedule(static)"]
         self.space = space
         self.params = self.space.keys()
-        self.starting_point = ["#pragma unroll"]
+        self.starting_point = ["#pragma omp parallel for private (pidx) schedule(static)","#pragma omp parallel for private (pidx) schedule(dynamic)"]
 
 if __name__ == '__main__':
     instance = Problem()
